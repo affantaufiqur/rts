2e1fdced2d9a04d9304d3147f37cec29910bf878
Affan <affantaufiqur@gmail.com>
2022-11-07 02:09:55 +0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="container mx-auto">
+      <div className="text-center font-semibold text-3xl">react + typescript + tailwind</div>
+    </div>
+  )
+}
+
+export default App
